e78af32aeccdd4ea06471939e980f7c4951900b5
scheck93 <109469032+scheck93@users.noreply.github.com>
2024-08-28 23:55:13 +0000

    new

--- src/models.py
+++ src/models.py
@@ -1,32 +1,72 @@
-import os
-import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+import enum
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum, Table
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class MediaType(enum.Enum):
+    audio = 1
+    video = 2
+    image = 3
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'User'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    username = Column(String(255), nullable=False)
+    firstname = Column(String(255), nullable=False)
+    lastname = Column(String(255), nullable=False)
+    email = Column(String(255), nullable=False)
 
-    def to_dict(self):
-        return {}
+class Post(Base):
+    __tablename__ = 'Post'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('User.id'))
+    user = relationship(User)
+
+class Media(Base):
+    __tablename__ = 'Media'
+
+    id = Column(Integer, primary_key=True)
+    type = Column('type', Enum(MediaType))
+    url = Column(String(255), nullable=False)
+    post_id = Column(Integer, ForeignKey('Post.id'))
+
+# follower_table = Table(
+#     "Follower",
+#     Base.metadata,
+#     Column("follower_id", ForeignKey("User.id"), primary_key=True),
+#     Column("followee_id", ForeignKey("User.id"), primary_key=True),
+# )
+
+class Follower(Base):
+    __tablename__ = 'Follower'
+
+    id = Column(Integer, primary_key=True)
+    follower_id = Column(Integer, ForeignKey('User.id'), nullable=False)
+    followee_id = Column(Integer, ForeignKey('User.id'), nullable=False)
+    
+    follower = relationship('User', back_populates='followers')
+    followee = relationship('User', back_populates='followees')
+
+class Comment(Base):
+    __tablename__ = 'Comment'
+
+    id = Column(Integer, primary_key=True)
+    author_id = Column(Integer, ForeignKey('User.id'), nullable=False)
+    post_id = Column(Integer, ForeignKey('Post.id'), nullable=False)
+    comment_text = Column(String(10000), nullable=False)
+    
+    user = relationship('User', back_populates='comments')
+    post = relationship('Post', back_populates='comments')
+
 
 ## Draw from SQLAlchemy base
-render_er(Base, 'diagram.png')
+try:
+    result = render_er(Base, 'diagram.png')
+    print("Success! Check the diagram.png file")
+except Exception as e:
+    print("There was a problem genering the diagram")
+    raise e
